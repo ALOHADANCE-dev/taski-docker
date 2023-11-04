acc7b4dd5ce93ff1d40d1dbcd06306201a7a0f91
Данила Удовиченко <danyarusskiy17@mail.ru>
2023-11-04 14:39:04 +0400

    nothing to say

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.14.35', '127.0.0.1', 'localhost', 'taski-laski.myddns.me']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'taski-laski.myddns.me']
 
 
 # Application definition
